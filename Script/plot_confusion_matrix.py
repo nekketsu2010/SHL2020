97ee705d7d04ea88e0296f3e1ef898e555d5f9d6
nekketsu2010 <41464917+nekketsu2010@users.noreply.github.com>
2020-04-09 18:42:32 +0900

    Create plot_confusion_matrix.py

---
+++ Script/plot_confusion_matrix.py
@@ -0,0 +1,35 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+import seaborn as sns
+sns.set(font='Yu Gothic')
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+def plot_confusion_matrix(test_y,pred_y,class_names,normalize=False):
+
+    cm = confusion_matrix(test_y,pred_y)
+    classes = class_names[unique_labels(test_y,pred_y)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes,
+           yticklabels=classes,
+           ylabel='True label\n',
+           xlabel='\nPredicted label')
+    fmt = '.2f' if normalize else 'd'
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j,
+                    i,
+                    format(cm[i, j], fmt),
+                    ha="center",
+                    va="center")
+    fig.tight_layout()
+    return ax
